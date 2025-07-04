d57019f7bbc4e114d33b6cb96c7d5704ecca8e70
javier69882 <javier69882@gmail.com>
2025-07-04 18:58:28 -0400

    config maeven

---
+++ gestor_torneos/src/test/java/Logico/DepositoTest.java
@@ -0,0 +1,28 @@
+package Logico;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DepositoTest {
+
+    @Test
+    void addElemento() {
+    }
+
+    @Test
+    void getElemento() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void vaciar() {
+    }
+
+    @Test
+    void getElementos() {
+    }
+}
